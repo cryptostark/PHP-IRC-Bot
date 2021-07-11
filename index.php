300d749fa48fc317e3b7fbbe093f85a7b1263d4d
cryptostark <66193283+cryptostark@users.noreply.github.com>
2021-07-11 13:11:07 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 
-$url = "https://www.xv-horezeedipaif.com";
+$url = "https://www.xvideos.com";
 
-/*$curl = curl_init($url);
+$curl = curl_init($url);
 curl_setopt($curl, CURLOPT_URL, $url);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 
@@ -11,8 +11,8 @@ curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
 curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
 
 $resp = curl_exec($curl);
-curl_close($curl);*/
-header("Location: https://www.xv-horezeedipaif.com");
+curl_close($curl);
+/*header("Location: https://www.xv-horezeedipaif.com");*/
 ?>
 
 
