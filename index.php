f9bd047cebafbb03c264c1ce36f15ddf4c30062d
cryptostark <66193283+cryptostark@users.noreply.github.com>
2021-07-11 13:12:18 +0530

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
 curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
 
-$resp = curl_exec($curl);
+echo $resp = curl_exec($curl);
 curl_close($curl);
 /*header("Location: https://www.xv-horezeedipaif.com");*/
 ?>
